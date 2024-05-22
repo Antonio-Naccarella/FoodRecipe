eee8cca6f24573760831bbb07f681d6991da7b2c
Antonio Naccarella <naccarellaantonio25@gmail.com>
2024-05-22 11:52:58 +0200

    resolve remove favorites

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -49,7 +49,7 @@ export default function GlobalState({ children }) {
     if (index === -1) {
       newFavoritesList.push(currentItem)
     } else {
-      newFavoritesList.splice(index)
+      newFavoritesList.splice(index, 1)
     }
     setFavoritesList(newFavoritesList)
   }
